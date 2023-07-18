2b130e9e3207bccc4cf2ed6df43df0ed8d3eb51f
javaLuo <376693576@qq.com>
2023-07-18 15:03:05 +0800

    update

--- public/blogs/live2d.js
+++ public/blogs/live2d.js
@@ -29,32 +29,36 @@ pnpm add pixi-live2d-display
     <canvas ref="liveCanvas" />
 </template>
 
-import * as PIXI from 'pixi.js';
-import { Live2DModel } from 'pixi-live2d-display/cubism4';
+<script>
+    import * as PIXI from 'pixi.js';
+    import { Live2DModel } from 'pixi-live2d-display/cubism4';
 
-window.PIXI = PIXI; // 为了pixi-live2d-display内部调用
+    window.PIXI = PIXI; // 为了pixi-live2d-display内部调用
 
-let app; // 为了存储pixi实例
-let model; // 为了存储live2d实例
+    let app; // 为了存储pixi实例
+    let model; // 为了存储live2d实例
 
-async mounted(){
-    app = new PIXI.Application({
-        view: this.$refs.liveCanvas,
-        autoStart: true,
-        resizeTo: window, 
-        backgroundAlpha: 0,
-    });
-
-    // 打包后live2d资源会出现在dist/下，这里用相对路径就能引用到了
-    model = await Live2DModel.from('./live/xxx.model3.json');
-
-    app.stage.addChild(model);
-    model.scale.set(0.1); // 调整缩放比例，一般原始资源尺寸非常大，需要缩小
-}
-
-beforeUnmount(){
-    model?.destroy(); // 销毁
-}
+    export default {
+        async mounted(){
+            app = new PIXI.Application({
+                view: this.$refs.liveCanvas,
+                autoStart: true,
+                resizeTo: window, 
+                backgroundAlpha: 0,
+            });
+    
+            // 打包后live2d资源会出现在dist/下，这里用相对路径就能引用到了
+            model = await Live2DModel.from('./live/xxx.model3.json');
+    
+            app.stage.addChild(model);
+            model.scale.set(0.1); // 调整缩放比例，一般原始资源尺寸非常大，需要缩小
+        }
+    
+        beforeUnmount(){
+            model?.destroy(); // 销毁
+        }
+    }
+</script>
 \`\`\`
 
 ### 5. 一些常用方法
